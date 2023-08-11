74c8c5651e736877a88d2f1230e74cc4823e8c5d
Tim Yao <tim.yao@dpc.vic.gov.au>
2023-08-11 22:12:40 +1000

    test nightwatch v3

---
+++ test/fixtures/intergration.steps.js
@@ -0,0 +1,39 @@
+const { Given, When, Then } = require('@cucumber/cucumber')
+
+Given('I visit the Google AU homepage', function () {
+  this.browser.url('https://google.com.au')
+})
+
+When('I check the page title', async function () {
+  this.pageTitle = await this.browser.getTitle()
+})
+
+Then('the title text should be {string}', async function (title) {
+  await this.browser.assert.equal(this.pageTitle, title)
+})
+
+Then('I should see the search form', async function () {
+  await this.browser.assert.visible('form[role="search"]')
+})
+
+Then('I should see {string}', async function (cssSelector) {
+  await this.browser.waitForElementVisible(cssSelector)
+})
+
+Then('I click {string}', async function (cssSelector) {
+  await this.browser.element(cssSelector).click()
+})
+
+Then('I run', async function (dataTable) {
+  const nightwatchCommands = dataTable.hashes()
+  for (let i=0; i < nightwatchCommands.length; i++) {
+    const nightwatchCommand = nightwatchCommands[i]
+    console.log('nightwatchCommand', nightwatchCommand)
+    await this.browser[nightwatchCommand.command](nightwatchCommand.arg)
+  }
+})
+
+Then('element commands should work', async function () {
+  const auLink = await this.browser.element.findAllByText('Australia')
+  this.browser.expect.element(auLink).text.to.equal('Australia')
+})
